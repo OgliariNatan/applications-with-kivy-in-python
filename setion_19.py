b4c1d4c4a5300e60df898c80e08ab85167f5f553
OgliariNatan <natanogliari@gmail.com>
2023-12-01 09:53:18 -0300

    h

--- setion_19.py
+++ setion_19.py
@@ -1,1 +1,9 @@
-kj
+#####	NOME:				setion_19.py
+#####	VERSÃO:				1.2
+#####	DESCRIÇÃO:			Implementação dos exercícios do curso de kivy
+#####	DATA DA CRIAÇÃO:	01/12/2023
+#####   ATUALIZADO EM:      xx/xx/xxxxxx
+#####	ESCRITO POR:		Natan Ogliari
+#####	E-MAIL:				natanogliari@gmail.com
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/OgliariNatan/applications-with-kivy-in-python/tree/main
