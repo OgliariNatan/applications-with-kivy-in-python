34b1dc36ca8e44846e8aab53a49e0a3e767aba8e
OgliariNatan <natanogliari@gmail.com>
2023-12-07 10:30:22 -0300

    add section22

---
+++ section_22.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+#####	NOME:				setion_22.py
+#####	VERSÃO:				1.2
+#####	DESCRIÇÃO:			Implementação dos exercícios do curso de kivy
+#####	DATA DA CRIAÇÃO:	01/12/2023
+#####   ATUALIZADO EM:      xx/xx/xxxxxx
+#####	ESCRITO POR:		Natan Ogliari
+#####	E-MAIL:				natanogliari@gmail.com
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/OgliariNatan/applications-with-kivy-in-python
+
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window #Para alterar o tamanho da janela de aoplicação
