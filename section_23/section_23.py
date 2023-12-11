7145e9d85e6779e9f2085c8f312b39d78a204cec
OgliariNatan <natanogliari@gmail.com>
2023-12-11 10:33:45 -0300

    ad examples

--- section_23/section_23.py
+++ section_23/section_23.py
@@ -10,9 +10,42 @@
 #####	PROJETO:			https://github.com/OgliariNatan/applications-with-kivy-in-python
 
 
+kv = """
+<Inicio@StackLayout>:
+    orientation: 'lr-tb'
+    spacing: 5
+    padding: 5
+
+Inicio:
+
+    Login:
+        id: popup
+        size_hint: 0.7, 0.4
+
+<Login@Popup>:
+
+    title: 'Login e Senha'
+"""
+
+
+
+
+
+import kivy
+kivy.require('1.9.1')
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window #Para alterar o tamanho da janela de aoplicação
+from kivy.lang import Builder
+
+class TelinhaApp(App):
+
+    def build(self):
+        return Builder.load_string(kv)
+
+TelinhaApp().run()
+
