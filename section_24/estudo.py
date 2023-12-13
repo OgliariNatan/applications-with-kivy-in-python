6a5f58acdd5c645aec7da71e7dc00ecf7908b197
OgliariNatan <natanogliari@gmail.com>
2023-12-13 10:59:48 -0300

    add estudo2.kv

--- section_24/estudo.py
+++ section_24/estudo.py
@@ -12,9 +12,8 @@ import kivy
 kivy.require('1.9.1')
 from kivy.app import App
 
-class Estudo1App(App):
-    pass
-
-
-janela = Estudo1App()
-janela.run()
+# class Estudo1App(App):
+#     pass
+#
+# janela = Estudo1App()
+# janela.run()
